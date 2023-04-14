8d1e0f29de154ee5c627ea2d9321f1a0a97d98df
ID-Emmett <1749987265@qq.com>
2023-04-15 07:48:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+// 请求资源
+try {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    'GET',
+    'https://id-emmett.github.io/topic/myRecord.js'
+  );
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsContent = xhr.responseText;
+      // 在这里处理js文件内容，将js内容转化为字符串
+      console.log(jsContent);
+    }
+  };
+  xhr.send();
+} catch (e) {
+  console.log(e);
+}
+
+
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header> -->
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
