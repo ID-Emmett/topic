18d5b1eaca7e9d6875fd4b229a513cdaef7b73d1
ID-Emmett <1749987265@qq.com>
2023-04-15 08:18:41 +0800

    fix: dist rename docs. use github pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: true,
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
